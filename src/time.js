29a16a2540c1b2c0a3fe77e35e99b3417b979db2
charlierudolph <charles.rudolph@originate.com>
2016-10-13 08:31:19 -0600

    update

--- src/time.js
+++ src/time.js
@@ -1,9 +1,17 @@
+let previousTimestamp
+
 const methods = {
-  Date,
-  setTimeout: setTimeout.bind(global),
+  beginTiming() {
+    previousTimestamp = getTimestamp()
+  },
+  clearInterval: clearInterval.bind(global),
   clearTimeout: clearTimeout.bind(global),
+  Date,
+  endTiming() {
+    return (getTimestamp() - previousTimestamp)
+  },
   setInterval: setInterval.bind(global),
-  clearInterval: clearInterval.bind(global)
+  setTimeout: setTimeout.bind(global)
 }
 
 if (typeof setImmediate !== 'undefined') {
@@ -11,20 +19,8 @@ if (typeof setImmediate !== 'undefined') {
   methods.clearImmediate = clearImmediate.bind(global)
 }
 
-
 function getTimestamp() {
   return new methods.Date().getTime()
-}
-
-let previousTimestamp
-
-methods.beginTiming = () => {
-  previousTimestamp = getTimestamp()
-}
-
-// Returns the interval from the previous call of beginTiming() to now in milliseconds
-methods.endTiming = () => {
-  return (getTimestamp() - previousTimestamp)
 }
 
 export default methods
